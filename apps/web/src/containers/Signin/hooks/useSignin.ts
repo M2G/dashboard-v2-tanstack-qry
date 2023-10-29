3013442976fcf4a7b4d6e51a89619d06d638189f
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-10-29 18:26:40 +0100

    feat: add signin hook

---
+++ apps/web/src/containers/Signin/hooks/useSignin.ts
@@ -0,0 +1,16 @@
+import { UseMutationResult, useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import signinUserService from '@/services/signin';
+import { queryClient } from '@/App';
+
+function useSignin(): UseMutationResult<AxiosResponse<any, any>> {
+  return useMutation({
+    mutationFn: signinUserService,
+    onSuccess: (): void => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['authenticate'] });
+    },
+  });
+}
+
+export default useSignin;
