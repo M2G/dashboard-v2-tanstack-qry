782d65c765d2c3158fe684a53c9519e98a04e5ef
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-11-19 23:57:10 +0100

    feat: code refacotr

--- apps/web/src/containers/Profil/Profil.tsx
+++ apps/web/src/containers/Profil/Profil.tsx
@@ -1,17 +1,17 @@
+import { useCallback, useContext } from 'react';
 import { AuthContext } from '@/AuthContext';
 import ProfilForm from '@/components/ProfilForm';
+
 import { useProfil, useProfilUpdate } from './hooks';
-import { useCallback, useContext } from 'react';
 
 function Profil(): JSX.Element | null {
   const { userData }: { userData: { id: number } } = useContext(AuthContext);
   const { data, isLoading } = useProfil({ id: userData?.id });
   const { mutate } = useProfilUpdate();
-  console.log('data data data data', data);
 
   const handleSubmit = useCallback(
-    (d: unknown): void => {
-      mutate({ ...d, id: userData?.id });
+    (updateProfilData: any): void => {
+      mutate({ ...updateProfilData, id: userData?.id });
     },
     [mutate, userData],
   );
